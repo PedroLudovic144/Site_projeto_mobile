e103d47ac3ba6c4efa58f768ebf0c3c2d98539c3
PedroLudovic144 <pedroludovic5@gmail.com>
2024-05-14 07:00:12 -0300

    New commit

--- Site (1)/Site/assets/js/js.js
+++
@@ -1,9 +0,0 @@
-  function dark(){
-    var element = document.body;
-    element.classList.toggle("darkTheme");
-  }
-
-  function font(){
-    var element = document.body;
-    element.classList.toggle("fontSize");
-  }
---
+++ js.js
@@ -0,0 +1,9 @@
+  function dark(){
+    var element = document.body;
+    element.classList.toggle("darkTheme");
+  }
+
+  function font(){
+    var element = document.body;
+    element.classList.toggle("fontSize");
+  }
